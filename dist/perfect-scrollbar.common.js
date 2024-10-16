7e90518fedd4b84acd56f0cc0e5ceccef4011c3f
Dimitar Todev <dimitar.todev@donatix.net>
2024-10-16 15:41:41 +0300

    passive handler check

--- dist/perfect-scrollbar.common.js
+++ dist/perfect-scrollbar.common.js
@@ -890,7 +890,9 @@ function wheel(i) {
     shouldPrevent = shouldPrevent || shouldPreventDefault(deltaX, deltaY);
     if (shouldPrevent && !e.ctrlKey) {
       e.stopPropagation();
-      e.preventDefault();
+      if (!e.passive) {
+        e.preventDefault();
+      }
     }
   }
 
